a9145aa352c086d5a3e6c7dcb60d5f70458ef8cd
Hussein Turfah <husseinturfah299@gmail.com>
2025-04-26 23:52:00 +0300

    Update dependencies and integrate Redux with persistence
    - Added new dependencies: @hello-pangea/dnd, @reduxjs/toolkit, axios, react-redux, and redux-persist.
    - Updated package.json and package-lock.json to reflect the new dependencies.
    - Integrated Redux store and persistence in main.jsx, wrapping the App component with Provider and PersistGate for state management.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.scss";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { store, persistor } from "./store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
