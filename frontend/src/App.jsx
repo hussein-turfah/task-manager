e515ccd89703e7b3b716d1090e4e434271daed2b
Hussein Turfah <husseinturfah299@gmail.com>
2025-04-27 03:11:03 +0300

    Add HistoryPage route and enhance Column component
    - Introduced HistoryPage route in App.jsx for navigation.
    - Updated Column component to use Droppable from @hello-pangea/dnd for drag-and-drop functionality.
    - Modified task rendering in Column to support dynamic task addition with TaskComposer.
    - Enhanced styling in Column's SCSS for better layout and task addition interface.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import NavBar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import BoardPage from "./pages/BoardPage";
+import HistoryPage from "./pages/HistoryPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<BoardPage />} />
+        <Route path="/history" element={<HistoryPage />} />
       </Routes>
     </div>
   );
--- frontend/src/components/Column/index.jsx
+++ frontend/src/components/Column/index.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import TaskCard from "../TaskCard";
 import styles from "./index.module.scss";
-function Column({ column, tasks }) {
+import { Droppable } from "@hello-pangea/dnd";
+import TaskComposer from "../TaskComposer";
+
+export default function Column({ column, tasks }) {
   return (
     <div className={styles.container}>
-      <h3>{column.title}</h3>
-      {tasks.map((task) => (
-        <TaskCard key={task.id} task={task} />
-      ))}
+      <h3>{column.name}</h3>
+      <Droppable droppableId={column.id.toString()}>
+        {(provided) => (
+          <div
+            className={styles.tasksContainer}
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {tasks?.map((task, index) => (
+              <TaskCard key={task?.id} task={task} index={index} />
+            ))}
+            {provided.placeholder}
+            <div className={styles.addTask}>
+              <TaskComposer columnId={column?.id} />
+            </div>
+          </div>
+        )}
+      </Droppable>
     </div>
   );
 }
-
-export default Column;
